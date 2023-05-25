4218c2222bb291c7e754345d6bdf8847a437c4c7
Kaliya Alkaline <68124391+KaliyaAlkaline@users.noreply.github.com>
2023-05-25 11:22:51 -0700

    Update remove.js

--- remove.js
+++ remove.js
@@ -4,9 +4,9 @@ const levels = JSON.parse(fs.readFileSync(path + ".levels", "utf8"))
 const slices = JSON.parse(fs.readFileSync("slices.json", "utf8"))
 const ping_ = Number(fs.readFileSync("ping_.json", "utf8").replace(/[^0-9]/g, ""))
 if (ping_ <= levels.length && ping_ > 0) {
-	var list = fs.readFileSync(path + "Resources\\LevelData.plist", "utf8")
+	let list = fs.readFileSync(path + "Resources\\LevelData.plist", "utf8")
 	levels.splice((ping_ - 1), 1)
-	var l = list.split("<string>").slice(1, -1)
+	let l = list.split("<string>").slice(1, -1)
 	for (let i = 0; i < l.length; i++) {
 		l[i] = l[i].split("</string>")[0]
 	}
@@ -14,8 +14,8 @@ if (ping_ <= levels.length && ping_ > 0) {
 	for (let i = 0; i < l.length; i++) {
 		l[i] = "<key>" + (i + 1) + "</key>\n<string>" + l[i] + "</string>"
 	}
-	var l = l.join("\n")
-	var list = slices[0] + l + slices[1]
+	let l = l.join("\n")
+	let list = slices[0] + l + slices[1]
 	fs.writeFileSync(path + ".levels", JSON.stringify(levels), "utf8")
 	fs.writeFileSync(path + "Resources\\LevelData.plist", list, "utf8")
 }
