1c905c5bed87b2fb09fccc1cc1d5ea6561f40349
Kaliya Alkaline <68124391+KaliyaAlkaline@users.noreply.github.com>
2023-05-25 12:20:15 -0700

    Update add.js

--- add.js
+++ add.js
@@ -3,7 +3,7 @@ const path = fs.readFileSync("path.lnk", "utf8").replace(/"/g, "").replace(/\//g
 const levels = JSON.parse(fs.readFileSync(path + ".levels", "utf8"))
 const slices = JSON.parse(fs.readFileSync("slices.json", "utf8"))
 const ping_ = JSON.parse(fs.readFileSync("ping_.json", "utf8"))
-let list = fs.readFileSync(path + "Resources\\LevelData.plist", "utf8")
+const list = fs.readFileSync(path + "Resources\\LevelData.plist", "utf8")
 ping_["name"] = ping_["name"].replace(/[^a-zA-Z0-9 ]/g, "")
 ping_["stars"] = Number(ping_["stars"].replace(/[^0-9\-]/g, ""))
 if (!ping_["song"].includes(".mp3")) {
@@ -20,10 +20,9 @@ if (ping_["name"] !== "" && ping_["stars"] !== "") {
 		l[i] = "<key>" + (i + 1) + "</key>\n<string>" + l[i] + "</string>"
 	}
 	l.push("<key>" + levels.length + "</key>\n<string></string>")
-	let l = l.join("\n")
-	let list = slices[0] + l + slices[1]
+	let list_ = slices[0] + l.join("\n") + slices[1]
 	fs.writeFileSync(path + ".levels", JSON.stringify(levels), "utf8")
-	fs.writeFileSync(path + "Resources\\LevelData.plist", list, "utf8")
+	fs.writeFileSync(path + "Resources\\LevelData.plist", list_, "utf8")
 } else {
 	console.log("There was an error trying to create your level. Either your level name or star value is incompatable with Geometry Dash.")
 }
